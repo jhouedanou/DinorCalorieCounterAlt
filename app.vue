a3abf89f0b3c3f4c0c900a9a79055c4c39ed9f4f
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-10-02 16:21:04 +0000

    mise en forme desktop

--- app.vue
+++ app.vue
@@ -1,52 +1,35 @@
 <template>
   <Splashpage v-if="showSplash" />
 
-  <div :class="{ 'overlay-active': showOverlay }">
-    <div class="yeleen container">
-      <div class="container mt-0 pt-6">
-        <div class="carz is-flex is-justify-content-space-between is-align-items-center">
-          <img src="/images/logo.png" class="same" alt="">
-          <p>Calculateur de calories</p>
-        </div>
-        <div class="tabs is-centered is-boxed is-toggle">
-          <ul class="tabs-list">
-            <li class="tab-item" :class="{ 'is-active': activeTab === 'aliments' }">
-              <a @click="activeTab = 'aliments'">Aliments populaires</a>
-            </li>
-            <li class="tab-item" :class="{ 'is-active': activeTab === 'calculateur' }">
-              <a @click="activeTab = 'calculateur'">Mon plat n'est pas dans la liste</a>
-            </li>
-            <div class="slider" :style="sliderStyle"></div>
-          </ul>
-
-        </div>
-
-        <RecetteComponent v-if="activeTab === 'aliments'" :aliments="aliments" />
-        <CalculateurCalories v-if="activeTab === 'calculateur'" />
+  <div class="yeleen container">
+    <div class="container mt-0 pt-6">
+      <div class="carz is-flex is-justify-content-space-between is-align-items-center">
+        <img src="/images/logo.png" class="same" alt="">
+        <p>Calculateur de calories</p>
       </div>
-    </div>
-    <div v-if="showOverlay" class="overlay">
-      <p>Pour une meilleure expérience, veuillez utiliser cette page dans l'application DinorApp<br>Disponible sur votre
-        mobile :</p><br>
-      <br>
-      <div class="store-links">
-        <a href="https://play.google.com/store/apps/details?id=com.bfedition.dinorapp&hl=fr" target="_blank">
-          <nuxt-img src="/images/googleplay.png" class="same" alt="" format="webp" quality="80" />
-        </a>
-        <a href="https://apps.apple.com/tr/app/dinor-app/id1515571306" target="_blank">
-          <nuxt-img src="/images/apple.png" class="same" alt="" format="webp" quality="80" />
-
-        </a>
+      <div class="tabs is-centered is-boxed is-toggle">
+        <ul class="tabs-list">
+          <li class="tab-item" :class="{ 'is-active': activeTab === 'aliments' }">
+            <a @click="activeTab = 'aliments'">Aliments populaires</a>
+          </li>
+          <li class="tab-item" :class="{ 'is-active': activeTab === 'calculateur' }">
+            <a @click="activeTab = 'calculateur'">Mon plat n'est pas dans la liste</a>
+          </li>
+          <div class="slider" :style="sliderStyle"></div>
+        </ul>
       </div>
+
+      <RecetteComponent v-if="activeTab === 'aliments'" :aliments="aliments" />
+      <CalculateurCalories v-if="activeTab === 'calculateur'" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRestaurants } from '@/composables/useRestaurants'
 import RecetteComponent from '@/components/RecetteComponent.vue'
-import RestaurantComponent from '@/components/RestaurantComponent.vue'
+import CalculateurCalories from '@/components/CalculateurCalories.vue'
 import Splashpage from '@/components/Splashpage.vue';
 
 const { restaurants } = useRestaurants()
@@ -67,18 +50,7 @@ onMounted(() => {
     showSplash.value = false;
   }, 7000);
 });
-//overlay pour les ordinateur s
-const showOverlay = ref(false)
-const isLargeScreen = computed(() => {
-  return window.innerWidth > 768
-})
 
-onMounted(() => {
-  showOverlay.value = isLargeScreen.value
-  window.addEventListener('resize', () => {
-    showOverlay.value = isLargeScreen.value
-  })
-})
 onMounted(async () => {
   const response = await fetch('/api/calories.json')
   const data = await response.json()
@@ -86,7 +58,12 @@ onMounted(async () => {
 })
 </script>
 
-<style scoped>
+<style>
+body {
+  /* max-width: 430px !important;
+  margin: 0 auto; */
+}
+
 .tabs-list {
   display: flex;
   justify-content: center;
@@ -118,12 +95,5 @@ onMounted(async () => {
   border-bottom-color: transparent;
   color: #000;
   font-weight: bold;
-}
-
-.overlay {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
 }
 </style>
