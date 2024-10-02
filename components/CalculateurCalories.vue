ab70a856f0c65c9d8534563c2f4e3ba4b6b2aed7
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-10-02 14:56:58 +0000

    specifier un plat

--- components/CalculateurCalories.vue
+++ components/CalculateurCalories.vue
@@ -14,38 +14,37 @@
                     <input v-model="searchTerm" placeholder="Rechercher un ingrédient" class="search-input">
                 </div>
 
-
-                <div class="ingredients-grid container columns is-mobile is-multiline">
-                    <div class="column is-6" v-for="ingredient in filteredIngredients" :key="ingredient.nom">
+                <div class="ingredients-grid columns is-mobile is-multiline">
+                    <div class="p-4 column is-4-mobile" v-for="ingredient in filteredIngredients" :key="ingredient.nom">
                         <div class="ingredient-item">
-                            <label class="ingredient-label mouff">
-                                <input type="checkbox" v-model="ingredientsSelectionnes" :value="ingredient"
-                                    class="emoji-checkbox">
-                                <span class="emoji-icon">{{ ingredient.icone }}</span>
+                            <label class="ingredient-label">
+                                <input type="checkbox" v-model="ingredient.selected" class="emoji-checkbox">
+                                <span class="emoji-icon">{{ ingredient.selected ? '✅' : ingredient.icone }}</span>
                                 <p class="zagba">{{ ingredient.nom }}</p>
                                 <p class="lerequin">{{ ingredient.calories }} cal</p>
                             </label>
-                            <input v-model="portions[ingredient.nom]" type="number" min="1"
-                                :disabled="!ingredientsSelectionnes.includes(ingredient)">
-                        </div>
+                            <input v-if="ingredient.selected" v-model="portions[ingredient.nom]" type="number" min="1"
+                                placeholder="">
 
+                        </div>
                     </div>
                 </div>
-                <button class="netflox" @click="calculerCalories">Calculer les calories</button>
-            </div>
-
-            <div v-if="etape === 3">
-                <h2 class="title">Résultat</h2>
-                <p>Recette : {{ nomRecette }}</p>
-                <p>Nombre total de calories : {{ totalCalories }}</p>
-                <button class="netflox" @click="reinitialiser">Nouvelle recette</button>
+                <div class="crofixed">
+                    <div class="total-calories" :class="{
+                        'vert': totalCalories >= 12000 && totalCalories <= 1500,
+                        'orange': totalCalories > 1500 && totalCalories <= 2200,
+                        'rouge': totalCalories > 2200
+                    }">
+                        Nombre total de calories : <span>{{ totalCalories }}</span> calories
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 export default {
     setup() {
@@ -58,30 +57,41 @@ export default {
         const searchTerm = ref('')
 
         const filteredIngredients = computed(() => {
-            return ingredients.value.filter(ingredient =>
+            const filtered = ingredients.value.filter(ingredient =>
                 ingredient.nom.toLowerCase().includes(searchTerm.value.toLowerCase())
-            )
+            );
+
+            // Éliminer les doublons
+            const uniqueFiltered = {};
+            filtered.forEach(ingredient => {
+                if (!uniqueFiltered[ingredient.nom]) {
+                    uniqueFiltered[ingredient.nom] = ingredient;
+                }
+            });
+
+            return Object.values(uniqueFiltered);
         })
+
         const calculerCalories = () => {
-            totalCalories.value = ingredientsSelectionnes.value.reduce((total, ingredient) => {
-                return total + ingredient.calories * (portions.value[ingredient.nom] || 1)
+            totalCalories.value = ingredients.value.reduce((total, ingredient) => {
+                if (ingredient.selected) {
+                    return total + ingredient.calories * (portions.value[ingredient.nom] || 1)
+                }
+                return total
             }, 0)
-            etape.value = 3
         }
 
-        const reinitialiser = () => {
-            etape.value = 1
-            nomRecette.value = ''
-            ingredientsSelectionnes.value = []
-            portions.value = {}
-            totalCalories.value = 0
-        }
+        watch([ingredients, portions], () => {
+            calculerCalories()
+        }, { deep: true })
 
         // Charger les ingrédients depuis le fichier JSON
         fetch('/api/calories.json')
             .then(response => response.json())
             .then(data => {
-                ingredients.value = data.aliments.flatMap(aliment => aliment.ingredients)
+                const allIngredients = data.aliments.flatMap(aliment => aliment.ingredients)
+                const uniqueIngredients = Array.from(new Set(allIngredients.map(JSON.stringify))).map(JSON.parse)
+                ingredients.value = uniqueIngredients
             })
 
         return {
@@ -92,13 +102,13 @@ export default {
             portions,
             totalCalories,
             calculerCalories,
-            reinitialiser,
             searchTerm,
             filteredIngredients
         }
     }
 }
 </script>
+
 <style lang="scss" scoped>
 $primary: #ec2f19;
 $secondary: #f3b702;
@@ -120,7 +130,6 @@ $secondary: #f3b702;
     font-size: 1rem;
 }
 
-
 .ingredients-flex {
     display: flex;
     flex-wrap: wrap;
@@ -137,11 +146,18 @@ $secondary: #f3b702;
 }
 
 .ingredient-label {
+    cursor: pointer;
     display: flex;
     flex-direction: column;
     align-items: center;
-    text-align: center;
-    margin-bottom: 0.5rem;
+}
+
+.emoji-checkbox {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
 }
 
 .emoji-icon {
@@ -149,9 +165,9 @@ $secondary: #f3b702;
     margin-bottom: 0.5rem;
 }
 
-.ingredient-name {
-    font-weight: bold;
-    margin-bottom: 0.25rem;
+.zagba,
+.lerequin {
+    margin: 0;
 }
 
 .ingredient-calories {
